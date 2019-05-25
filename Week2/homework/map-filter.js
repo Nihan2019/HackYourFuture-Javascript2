10de31d29bb37230cffe755685845c4fa7c981b2
Nihan2019 <48648228+Nihan2019@users.noreply.github.com>
2019-05-25 13:50:53 +0200

    Add files via upload

--- Week2/homework/map-filter.js
+++ Week2/homework/map-filter.js
@@ -1,13 +1,14 @@
 'use strict';
 
 function doubleOddNumbers(numbers) {
-  // Replace this comment and the next line with your code
-  console.log(numbers);
-}
+  const oddNumbers = numbers.filter(numbers => numbers % 2 !== 0);// Replace this comment and the next line with your code
+  const doubleOddNumbers = oddNumbers.map(oddNumbers => oddNumbers * 2);
 
+  console.log(doubleOddNumbers);// Replace this comment and the next line with your code
+
+}
 const myNumbers = [1, 2, 3, 4];
 console.log(doubleOddNumbers(myNumbers));
-
 // Do not change or remove anything below this line
 module.exports = {
   myNumbers,
